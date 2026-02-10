c6cd399a2dd0f267b67399e301a47eac3edc58df
Pachandre <v.andrey-cher@yandex.ru>
2026-02-10 23:57:30 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-chat-devs/service-core
+
+go 1.25.4
---
+++ internal/storage/storage.go
@@ -0,0 +1,7 @@
+package storage
+
+import "github.com/go-chat-devs/service-core/internal/storage/users"
+
+type Storage struct {
+	Users *users.Storage
+}
