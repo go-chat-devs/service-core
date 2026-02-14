63db03f02a45560daa44e897bb239851dbe4a403
zxCroshka <rusmaned@gmail.com>
2026-02-14 12:11:01 +0300

    fix user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,12 +1,17 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+)
 
-
-type User struct{
-	id int64
-	uuid string
-	username string
-	avatarUuid string
+type User struct {
+	ID        int64
+	UserUID   uuid.UUID
+	Username  uuid.UUID
+	AvatarUID uuid.UUID
 }
 
-
+func (u *User) FromRow(row pgx.Row) error {
+	return row.Scan(&u.ID, &u.UserUID, &u.Username, &u.AvatarUID)
+}
