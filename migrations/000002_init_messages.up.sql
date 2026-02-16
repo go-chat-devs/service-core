5b1a6d97798b6e7679a603a309d0e25537be0992
Pachandre <v.andrey-cher@yandex.ru>
2026-02-16 17:31:35 +0300

    fix(migrations): messages

--- migrations/000002_init_messages.up.sql
+++ migrations/000002_init_messages.up.sql
@@ -2,23 +2,22 @@ CREATE TYPE MESSAGE_TYPE AS ENUM ('text', 'image');
 
 CREATE TABLE IF NOT EXISTS messages (
 	id SERIAL PRIMARY KEY,
-	uid UUID NOT NULL UNIQUE,
 	chat_uid UUID NOT NULL,
 	type MESSAGE_TYPE NOT NULL,
 	sent_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS text_messages (
-	id SERIAL PRIMARY KEY,
-	message_uid UUID NOT NULL UNIQUE REFERENCES messages(uid) ON DELETE CASCADE,
+	uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	message_id INT REFERENCES messages(id) ON DELETE CASCADE,
 	content TEXT,
 	user_uid UUID,
 	changed_at TIMESTAMPTZ
 );
 
 CREATE TABLE IF NOT EXISTS image_messages (
-	id SERIAL PRIMARY KEY,
-	message_uid UUID NOT NULL UNIQUE REFERENCES messages(uid) ON DELETE CASCADE,
+	uid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	message_id INT REFERENCES messages(id) ON DELETE CASCADE,
 	file_uid UUID NOT NULL DEFAULT gen_random_uuid(),
 	user_uid UUID
 );
